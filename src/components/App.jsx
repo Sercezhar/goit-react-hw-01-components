6b5e2142991f0dff723aaa45a39eefdba843d765
Oleksandr Havryliuk <deolis.ua@gmail.com>
2022-04-29 22:01:45 +0300

    task-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,16 @@
+import { SocialProfile } from './SocialProfile/SocialProfile';
+import user from './SocialProfile/data/user.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <SocialProfile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
     </div>
   );
 };
